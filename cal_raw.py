b01b331ad47895e9964dbc341fa7ef8bec1b5aa6
viljo690 <viljo690@su02-209.ad.liu.se>
2024-10-10 15:01:09 +0200

    added architecture

---
+++ cal_raw.py
@@ -0,0 +1,21 @@
+
+
+class UnbuiltCal:
+    pass
+
+
+class SrcCal:
+    pass
+
+
+class Filter:
+    pass
+
+
+class Pattern:
+    pass
+
+
+class Action:
+    pass
+
---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+if __name__ == '__main__':
+    # create cal raw
+    # add src calendars
+    # add filters
+    # build cal
+    # display cal (print)

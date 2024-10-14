82e1e216373d509ef6725086f7b2017a0a8e3304
ToglivvilgoT <vilgot.johansen@hotmail.com>
2024-10-14 11:51:17 +0200

    added ActionMultiple class

--- cal.py
+++ cal.py
@@ -1,15 +1,15 @@
 from typing import Iterable
 
 
-class Calendar:
-    """ Class for Calendars """
-    def __init__(self, events: Iterable[Event]):
-        """ Initializes Calendar Object """
-        self.events = [event for event in events]
-
-
 class Event:
     """ Class for Calendar Events """
     def __init__(self, content: dict[str, str]):
         """ content keys should be fields and content values is the text of that field """
         self.content = content
+
+
+class Calendar:
+    """ Class for Calendars """
+    def __init__(self, events: Iterable[Event]):
+        """ Initializes Calendar Object """
+        self.events = [event for event in events]
--- cal_raw.py
+++ cal_raw.py
@@ -4,23 +4,43 @@ from abc import ABC, abstractmethod
 from cal import Calendar, Event
 
 
-class SrcCal:
+class SrcCal(ABC):
     @abstractmethod
     def get_events() -> Iterable[Event]:
         pass
 
 
-class Pattern:
+class Pattern(ABC):
     @abstractmethod
     def resolve(event: Event) -> bool:
         pass
 
 
 class Action(ABC):
+    """ ABC for actions which are applied to events """
     @abstractmethod
     def resolve(event: Event) -> list[Event]:
         pass
 
+
+class ActionMultiple(Action):
+    """ Class for handling multiple actions in sequence """
+    def __init__(self, actions: Iterable[Action]):
+        self.actions = [action for action in actions]
+
+    def resolve(self, event: Event):
+        current_events = [event]
+        next_events = []
+
+        for action in self.actions:
+            for event in current_events:
+                for next_event in action.resolve(event):
+                    next_events.append(next_event)
+
+            current_events = next_events
+            next_events = []
+
+            
 
 class Filter:
     """ Class for Event Filters """

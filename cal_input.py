a956c711785d9b318235aa3729f45708e9a896d7
viljo690 <viljo690@su11-206.ad.liu.se>
2024-10-15 10:07:03 +0200

    added SrcCalURL

---
+++ cal_input.py
@@ -0,0 +1,50 @@
+import requests
+
+from cal import Calendar, Event
+from cal_raw import SrcCal
+
+
+class SrcCalURL(SrcCal):
+    def __init__(self, url: str):
+        self.url = url
+        self.cal = None
+
+    def read_ics(self, file_raw: str):
+        file_raw = file_raw.replace('\r\n ', '')
+
+        calendars: list[list[dict[str, str]]] = []
+        
+        for line in file_raw.split('\r\n'):
+            if not line:
+                continue
+
+            print(line)
+            field, content = line.split(':', 1)
+            print(field, content)
+
+    def fetch(self):
+        """ loads in all content from the url """
+        
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raises an HTTPError if the response code was unsuccessful
+        except requests.exceptions.HTTPError as http_err:
+            raise Exception(f"HTTP error occurred: {http_err}")  # Could be a 404 or 500 error, for example
+        except requests.exceptions.ConnectionError as conn_err:
+            raise Exception(f"Connection error occurred: {conn_err}")
+        except requests.exceptions.Timeout as timeout_err:
+            raise Exception(f"Timeout error occurred: {timeout_err}")
+        except requests.exceptions.RequestException as req_err:
+            raise Exception(f"An error occurred: {req_err}")
+
+        file_raw = response.content.decode('utf-8')
+
+        self.cal = self.read_ics(file_raw)
+
+    def get_events():
+        pass
+
+
+if __name__ == '__main__':
+    src_cal = SrcCalURL('https://cloud.timeedit.net/liu/web/schema/ri647QQQY80Zn1Q5368009Z8y6Z56.ics')
+    src_cal.fetch()
